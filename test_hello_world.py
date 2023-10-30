1cefd1d1990459e4d9ba84785c975e4504ffe9c4
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-10-30 23:10:50 +0530

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,7 +1,8 @@
 import unittest
-from helloworld import say_hello  # Import the function to be tested
+from hello_world import say_hello  # Import the function to be tested
 
 class TestHelloWorld(unittest.TestCase):
+
     def test_hello_world_presence(self):
         result = say_hello()
         self.assertIn("Hello World!", result)
