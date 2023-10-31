f2ab2810638ebbe061c1c2a4dbbdf8dc4a63ffa5
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-10-31 11:33:25 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ from io import StringIO
 import sys
 
 def hello_world():
-    return "gta!"
+    return "Hello World!"
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world_output(self):
@@ -20,7 +20,7 @@ class TestHelloWorld(unittest.TestCase):
         output = captured_output.getvalue().strip()
 
         # Perform the test
-        self.assertEqual(output, "gta!")
+        self.assertEqual(output, "Hello World!")
 
 if __name__ == '__main__':
     unittest.main()
