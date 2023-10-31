e8cccf2e2de8fa71afcaa6e427c76ef0f76be29d
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-10-31 18:42:38 +0530

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -7,12 +7,13 @@ class TestHelloWorld(unittest.TestCase):
     def test_hello_world_output(self):
         # Call the hello_world function and capture the result
         result = hello_world()
-        
-        # Print the result, which will be displayed in the workflow summary
-        print(result)
-        
+
         # Perform the test
-        self.assertEqual(result, "Hello World!")  # Note the change to "Hello, World!" here
+        self.assertEqual(result, "Hello, World!")  # Note the change to "Hello, World!" here
+
+        # If the test case fails, print "gta"
+        if result != "Hello, World!":
+            print("gta")
 
 if __name__ == '__main__':
     unittest.main()
