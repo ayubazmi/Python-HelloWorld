77948e8d941883c6937694ccb0cfcc88a5f04820
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-10-31 19:38:32 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,26 +1,5 @@
-import unittest
-from io import StringIO
-import sys
+def say_hello():
+    return "Hello, World!"
 
-def hello_world():
-    return "Hello World!"
-
-class TestHelloWorld(unittest.TestCase):
-    def test_hello_world_output(self):
-        # Redirect stdout to capture the output
-        captured_output = StringIO()
-        sys.stdout = captured_output
-
-        hello_world()
-
-        # Reset stdout
-        sys.stdout = sys.__stdout__
-
-        # Get the captured output
-        output = captured_output.getvalue().strip()
-
-        # Perform the test
-        self.assertEqual(output, "Hello, World!")
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    print(say_hello())
