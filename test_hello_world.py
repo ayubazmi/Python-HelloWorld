9a20635fb2bcbe2d0ec1d592dd3f39e7ee62ea6e
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-10-31 11:27:51 +0530

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,40 +1,18 @@
-name: Python application
+import unittest
 
-on:
-  push:
-    branches: [ "master" ]
-  pull_request:
-    branches: [ "master" ]
+def hello_world():
+    return "Hello, World!"
 
-permissions:
-  contents: read
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world_output(self):
+        # Call the hello_world function and capture the result
+        result = hello_world()
+        
+        # Print the result, which will be displayed in the workflow summary
+        print(result)
+        
+        # Perform the test
+        self.assertEqual(result, "Hello, World!")
 
-jobs:
-  build:
-
-    runs-on: ubuntu-latest
-
-    steps:
-    - uses: actions/checkout@v2
-    - name: Set up Python 3.10
-      uses: actions/setup-python@v2
-      with:
-        python-version: "3.10"
-    - name: Install dependencies
-      run: |
-        python -m pip install --upgrade pip
-        pip install flake8 pytest
-        if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
-    - name: Install xmlrunner
-      run: |
-        pip install xmlrunner
-    - name: Lint with flake8
-      run: |
-        # stop the build if there are Python syntax errors or undefined names
-        flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
-        # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-        flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
-    - name: Test with pytest
-      run: |
-        # Redirect the "Hello, World!" output to the standard output
-        python -m pytest -s
+if __name__ == '__main__':
+    unittest.main()
