ba2b1ce50c21fb023554fdd8da201f5ac97ec282
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-10-31 11:18:35 +0530

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,12 +1,18 @@
 import unittest
-import xmlrunner
 
 def hello_world():
     return "Hello, World!"
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world_output(self):
-        self.assertEqual(hello_world(), "Hello, World!")
+        # Call the hello_world function and capture the result
+        result = hello_world()
+        
+        # Print the result, which will be displayed in the workflow summary
+        print(result)
+        
+        # Perform the test
+        self.assertEqual(result, "Hello, World!")
 
 if __name__ == '__main__':
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    unittest.main()
